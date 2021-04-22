c7d69401a413a39593111b94c306a49880456f72
Daijiro MORI <daijiro.mori@gmail.com>
2021-04-22 17:17:25 +0900

    Fix a typo

--- eclosor/embed/__init__.py
+++ eclosor/embed/__init__.py
@@ -130,7 +130,7 @@ class EmbedRecommender:
         res = [(cos, id) for cos, id in res if cos >= score_threshold]
         res.sort(reverse=True)
         ids = [id for cos, id in res[:limit]]
-        scores = [score for cos, id in res[:limit]]
+        scores = [cos for cos, id in res[:limit]]
         return ids, scores
 
     def is_known_user(self, user_id):
